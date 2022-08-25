7cbe532c68dbf5f5afa4ab3f06127620383a4386
abdisag1 <myabdisa@gmail.com>
2022-08-25 17:35:42 +0300

    [test contious deploymet]

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import logo from './Holberton_logo.jpg';
+
 import './App.css';
-import { getFullYear, getFooterCopy } from './utils';
+
 
 function App() {
 	return (
 		<div className='App'>
 			<div className='App-header'>
-				<img src={logo} alt='Holberton' />
-				<h1>School dashboard</h1>
+				
+				<h1>buid it App Home page</h1>
 			</div>
 			<div className='App-body'>
-				<p>Login to access the full dashboard</p>
+				
 			</div>
 			<div className='App-footer'>
 				<p>
-					Copyright {getFullYear()} - {getFooterCopy(true)}
+					
 				</p>
 			</div>
 		</div>
